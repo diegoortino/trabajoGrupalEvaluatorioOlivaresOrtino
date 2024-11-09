6e8200a1cabd1f2876feb259578b725081c049dd
diegoortino <166074675+diegoortino@users.noreply.github.com>
2024-11-09 19:09:59 -0300

    Commit del 9/11

--- app.js
+++ app.js
@@ -54,6 +54,9 @@ var Variacion1 = /** @class */ (function (_super) {
     Variacion1.prototype.apostarTodo = function (saldo) {
         throw new Error("Method not implemented.");
     };
+    Variacion1.prototype.toString = function () {
+        return this.nombreDelJuego;
+    };
     return Variacion1;
 }(Tragamonedas));
 exports.Variacion1 = Variacion1;
@@ -74,6 +77,9 @@ var Variacion2 = /** @class */ (function (_super) {
     Variacion2.prototype.calcularResultado = function (apuesta) {
         throw new Error("Method not implemented.");
     };
+    Variacion2.prototype.toString = function () {
+        return this.nombreDelJuego;
+    };
     return Variacion2;
 }(Tragamonedas));
 exports.Variacion2 = Variacion2;
@@ -85,16 +91,37 @@ var Ruleta = /** @class */ (function (_super) {
         return _this;
     }
     Ruleta.prototype.jugar = function (apuesta, numeroElegido) {
-        throw new Error("Method not implemented.");
+        this.ganador = 2 /*Math.floor(Math.random() * this.numeros)*/;
+        if (numeroElegido === this.ganador) {
+            this.esGanador = true;
+            return "\u00A1Felicidades! Has ganado la apuesta. El n\u00FAmero ganador fue ".concat(this.ganador, ".");
+        }
+        else {
+            this.esGanador = false;
+            return "Lo siento, perdiste. El n\u00FAmero ganador fue ".concat(this.ganador, ".");
+        }
     };
-    Ruleta.prototype.apostarTodo = function (saldo) {
-        throw new Error("Method not implemented.");
+    Ruleta.prototype.apostarTodo = function (saldo, numeroElegido) {
+        this.ganador = Math.floor(Math.random() * this.numeros);
+        if (numeroElegido === this.ganador) {
+            this.esGanador = true;
+            return "\u00A1Felicidades! Has ganado la apuesta. El n\u00FAmero ganador fue ".concat(this.ganador, ".");
+        }
+        else {
+            this.esGanador = false;
+            return "Lo siento, perdiste. El n\u00FAmero ganador fue ".concat(this.ganador, ".");
+        }
     };
     Ruleta.prototype.verResultado = function () {
-        throw new Error("Method not implemented.");
+        return this.esGanador;
     };
     Ruleta.prototype.calcularResultado = function (apuesta) {
-        throw new Error("Method not implemented.");
+        if (this.esGanador === false) {
+            return -apuesta;
+        }
+        else {
+            return apuesta * 37;
+        }
     };
     return Ruleta;
 }(Juego));
@@ -149,12 +176,16 @@ var CarreraDeCaballos = /** @class */ (function (_super) {
     CarreraDeCaballos.prototype.calcularResultado = function (apuesta) {
         throw new Error("Method not implemented.");
     };
+    CarreraDeCaballos.prototype.toString = function () {
+        return this.nombreDelJuego;
+    };
     return CarreraDeCaballos;
 }(Juego));
 exports.CarreraDeCaballos = CarreraDeCaballos;
 var Jugador = /** @class */ (function () {
     function Jugador(nombre, edad) {
-        this.saldo = 1000;
+        this.dinero = 1000;
+        this.fichas = 0;
         this.nombre = nombre;
         this.edad = edad;
     }
@@ -164,17 +195,24 @@ var Jugador = /** @class */ (function () {
     Jugador.prototype.getEdad = function () {
         return this.edad;
     };
-    Jugador.prototype.getSaldo = function () {
-        return this.saldo;
+    Jugador.prototype.getDinero = function () {
+        return this.dinero;
     };
-    Jugador.prototype.modificarSaldo = function (cantidad) {
-        this.saldo += cantidad;
+    Jugador.prototype.getFichas = function () {
+        return this.fichas;
+    };
+    Jugador.prototype.setDinero = function (nuevoValorDinero) {
+        this.dinero = nuevoValorDinero;
+    };
+    Jugador.prototype.setFichas = function (nuevoValorFichas) {
+        this.fichas = nuevoValorFichas;
     };
     return Jugador;
 }());
 exports.Jugador = Jugador;
 var Casino = /** @class */ (function () {
     function Casino(nombreCasino) {
+        this.estaAbierto = false;
         this.jugadores = [];
         this.juegos = [];
         this.nombreCasino = nombreCasino;
@@ -182,32 +220,110 @@ var Casino = /** @class */ (function () {
     Casino.prototype.getNombreCasino = function () {
         return this.nombreCasino;
     };
-    Casino.prototype.agregarJugador = function (nombre, edad) {
-        var jugador = new Jugador(nombre, edad);
+    Casino.prototype.abrirCasino = function () {
+        if (!this.estaAbierto) {
+            this.estaAbierto = true;
+        }
+        else {
+            this.estaAbierto = false;
+        }
+        ;
+    };
+    Casino.prototype.cerrarCasino = function () {
+        if (this.estaAbierto) {
+            this.estaAbierto = false;
+        }
+        else {
+            this.estaAbierto = true;
+        }
+        ;
+    };
+    Casino.prototype.verMayoriaEdad = function (jugador) {
         if (jugador.getEdad() >= 18) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Casino.prototype.cambiarDineroPorFichas = function (jugador, valor) {
+        var dineroDelJugador = jugador.getDinero();
+        var fichasDelJugador = jugador.getFichas();
+        if (valor > 0 && dineroDelJugador >= valor) {
+            dineroDelJugador -= valor;
+            fichasDelJugador += valor;
+            jugador.setDinero(dineroDelJugador);
+            jugador.setFichas(fichasDelJugador);
+        }
+        else {
+            jugador.setDinero(dineroDelJugador);
+            jugador.setFichas(fichasDelJugador);
+        }
+    };
+    Casino.prototype.cobrarLaCaja = function (jugador, valor) {
+        var dineroDelJugador = jugador.getDinero();
+        var fichasDelJugador = jugador.getFichas();
+        if (valor > 0 && fichasDelJugador >= valor) {
+            dineroDelJugador += valor;
+            fichasDelJugador -= valor;
+            jugador.setDinero(dineroDelJugador);
+            jugador.setFichas(fichasDelJugador);
+        }
+        else {
+            jugador.setDinero(dineroDelJugador);
+            jugador.setFichas(fichasDelJugador);
+        }
+    };
+    Casino.prototype.agregarJugador = function (jugador) {
+        if (this.verMayoriaEdad(jugador)) {
             this.jugadores.push(jugador);
             return true;
         }
         else {
-            console.log("".concat(jugador.getNombre(), " no es mayor de edad y no puede jugar en el casino."));
             return false;
         }
-    };
-    Casino.prototype.getSaldoJugador = function () {
-        return this.jugadores[0].getSaldo();
     };
     Casino.prototype.agregarJuego = function (JuegoClase) {
         var juego = new JuegoClase();
         this.juegos.push(juego);
     };
-    Casino.prototype.listarJuegos = function () {
-        this.juegos.forEach(function (juego, index) {
-            console.log("".concat(index + 1, ". ").concat(juego["nombreDelJuego"]));
-        });
+    Casino.prototype.modificarFichas = function (jugador, resultado) {
+        var nuevoValor = jugador.getFichas() + resultado;
+        jugador.setFichas(nuevoValor);
     };
-    Casino.prototype.jugarJuego = function (opcionElegida, apuesta, parametroAdicional) {
+    Casino.prototype.jugarJuego = function (jugador, numeroDeJuego, apuesta, parametroAdicional) {
+        if (numeroDeJuego >= 0 && numeroDeJuego < this.juegos.length) {
+            if (jugador.getFichas() >= apuesta) {
+                var juegoSeleccionado = this.juegos[numeroDeJuego];
+                var jugar = juegoSeleccionado.jugar(apuesta, parametroAdicional);
+                var resultado = juegoSeleccionado.calcularResultado(apuesta);
+                this.modificarFichas(jugador, resultado);
+                return jugar;
+            }
+            else {
+                return "No tenes suficientes fichas";
+            }
+        }
+        else {
+            return "a";
+        }
     };
-    Casino.prototype.apostarTodo = function (opcionElegida, apuesta, parametroAdicional) {
+    Casino.prototype.jugarApostandoTodo = function (jugador, numeroDeJuego, fichas, parametroAdicional) {
+        if (numeroDeJuego >= 0 && numeroDeJuego < this.juegos.length) {
+            if (jugador.getFichas() >= fichas) {
+                var juegoSeleccionado = this.juegos[numeroDeJuego];
+                var jugar = juegoSeleccionado.jugar(fichas, parametroAdicional);
+                var resultado = juegoSeleccionado.calcularResultado(fichas);
+                this.modificarFichas(jugador, resultado);
+                return jugar;
+            }
+            else {
+                return "No tenes suficientes fichas";
+            }
+        }
+        else {
+            return "a";
+        }
     };
     return Casino;
 }());
--- main.js
+++ main.js
@@ -3,17 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var readlineSync = require("readline-sync");
 var app_1 = require("./app");
 var CasinoMain = new app_1.Casino("Lucky 38 de New Vegas");
+var jugadorActual = null;
 CasinoMain.agregarJuego(app_1.Variacion1);
 CasinoMain.agregarJuego(app_1.Variacion2);
 CasinoMain.agregarJuego(app_1.Ruleta);
 CasinoMain.agregarJuego(app_1.CarreraDeCaballos);
-function menuPrincipal() {
-    console.clear();
-    mensajeCentrado("Usted esta en el lobby del casino " + CasinoMain.getNombreCasino());
-    console.log("Juegos disponibles en el casino:");
-    CasinoMain.listarJuegos();
-    console.log("5. Salir");
-}
 function mensajeCentrado(mensaje) {
     var anchoDeConsola = process.stdout.columns || 80;
     var padding = Math.max(0, Math.floor((anchoDeConsola - mensaje.length) / 2));
@@ -25,22 +19,166 @@ function registrarse() {
     while (!registrado) {
         var nombre = readlineSync.question('Nombre: ');
         var edad = readlineSync.questionInt('Edad: ');
-        registrado = CasinoMain.agregarJugador(nombre, edad);
+        registrado = CasinoMain.agregarJugador(new app_1.Jugador(nombre, edad));
         if (registrado) {
+            jugadorActual = new app_1.Jugador(nombre, edad);
+            console.log();
             console.log("Jugador registrado exitosamente en el casino.");
             menuPrincipal();
         }
         else {
-            console.log("Registro fallido. Inténtalo nuevamente.");
+            console.log("Registro fallido. Solo las personas con mayoria de edad pueden apostar.");
             console.log();
         }
     }
 }
+function menuRuleta() {
+    if (jugadorActual) {
+        mensajeCentrado("Bienvenido a la Ruleta");
+        console.log("Fichas: " + jugadorActual.getFichas());
+        console.log();
+        console.log("1. Apostar un monto");
+        console.log("2. Apostar todo");
+        console.log("3. Volver al menu de juegos");
+        var opcion = readlineSync.questionInt("Ingrese: ");
+        switch (opcion) {
+            case 1:
+                console.clear();
+                var apuesta = parseInt(readlineSync.question("¡Cuantas fichas desea apostar? ingrese: "));
+                console.log(CasinoMain.jugarJuego(jugadorActual, 2, apuesta, 2));
+                menuRuleta();
+            case 2:
+                console.clear();
+                var fichas = jugadorActual.getFichas();
+                console.log(CasinoMain.jugarApostandoTodo(jugadorActual, 2, fichas, 2));
+                menuRuleta();
+            case 3:
+                console.clear();
+                menuJuegos();
+            default:
+                console.clear();
+                console.log("Opción inválida. Seleccione nuevamente");
+                menuRuleta();
+                break;
+        }
+    }
+    else {
+        console.log("Error: No hay un jugador registrado.");
+        terminarJuego();
+    }
+}
+function menuCarreraDeCaballos() {
+    if (jugadorActual) {
+        mensajeCentrado("Bienvenido a la Carrera de Caballos");
+        console.log("Dinero: " + jugadorActual.getDinero());
+        console.log("Fichas: " + jugadorActual.getFichas());
+        console.log();
+    }
+    else {
+        console.log("Error: No hay un jugador registrado.");
+        terminarJuego();
+    }
+}
+function menuTragamonedas() {
+    if (jugadorActual) {
+        mensajeCentrado("Bienvenido a las Tragamonedas");
+        console.log("Dinero: " + jugadorActual.getDinero());
+        console.log("Fichas: " + jugadorActual.getFichas());
+        console.log();
+    }
+    else {
+        console.log("Error: No hay un jugador registrado.");
+        terminarJuego();
+    }
+}
+function menuJuegos() {
+    mensajeCentrado("Juegos disponibles en el casino:");
+    console.log();
+    console.log("1. Tragamonedas");
+    console.log("2. Carrera de Caballos");
+    console.log("3. Ruleta");
+    console.log("4. Volver al menu principal");
+    var opcion = readlineSync.questionInt("Ingrese: ");
+    switch (opcion) {
+        case 1:
+            console.clear();
+            menuTragamonedas();
+            break;
+        case 2:
+            console.clear();
+            menuCarreraDeCaballos();
+            break;
+        case 3:
+            console.clear();
+            menuRuleta();
+            break;
+        case 4:
+            console.clear();
+            menuPrincipal();
+            return;
+        default:
+            console.clear();
+            console.log("Opción inválida. Seleccione nuevamente");
+            menuInicial();
+            break;
+    }
+}
+function menuPrincipal() {
+    if (jugadorActual) {
+        console.log();
+        mensajeCentrado("Usted está en el lobby del casino " + CasinoMain.getNombreCasino());
+        console.log("Su dinero es de: " + jugadorActual.getDinero() + "$");
+        console.log("Sus fichas son: " + jugadorActual.getFichas());
+        console.log();
+        console.log("1. Jugar");
+        console.log("2. Comprar Fichas");
+        console.log("3. Cambiar fichas por dinero");
+        console.log("4. Salir");
+        var opcion = readlineSync.questionInt("Ingrese: ");
+        switch (opcion) {
+            case 1:
+                console.clear();
+                menuJuegos();
+                break;
+            case 2:
+                console.clear();
+                console.log("¿Cuantas fichas desea comprar?");
+                var fichasAComprar = parseInt(readlineSync.question("Ingrese: "));
+                CasinoMain.cambiarDineroPorFichas(jugadorActual, fichasAComprar);
+                console.clear();
+                console.log();
+                console.log("Operacion Realizada");
+                menuPrincipal();
+                break;
+            case 3:
+                console.clear();
+                console.log("¿Cuantas fichas desea cambiar?");
+                var fichasAVender = parseInt(readlineSync.question("Ingrese: "));
+                CasinoMain.cobrarLaCaja(jugadorActual, fichasAVender);
+                console.clear();
+                console.log();
+                console.log("Operacion Realizada");
+                menuPrincipal();
+                break;
+            case 4:
+                terminarJuego();
+                break;
+            default:
+                console.clear();
+                console.log("Opción inválida. Seleccione nuevamente");
+                menuPrincipal();
+                break;
+        }
+    }
+    else {
+        console.log("Error: No hay un jugador registrado.");
+        terminarJuego();
+    }
+}
 function menuInicial() {
-    console.clear();
     console.log();
     mensajeCentrado("Bienvenido al Casino " + CasinoMain.getNombreCasino());
-    console.log("");
+    console.log();
     console.log("1. Ingresar");
     console.log("2. Salir");
     console.log("");
@@ -52,8 +190,7 @@ function menuInicial() {
             break;
         case 2:
             console.clear();
-            console.log("Esperamos verte pronto");
-            process.exit();
+            terminarJuego();
         default:
             console.clear();
             console.log("Opción inválida. Seleccione nuevamente");
@@ -62,6 +199,14 @@ function menuInicial() {
     }
 }
 function iniciarJuego() {
+    CasinoMain.abrirCasino();
     menuInicial();
+}
+function terminarJuego() {
+    console.clear();
+    console.log();
+    console.log("Esperamos verte pronto");
+    CasinoMain.cerrarCasino();
+    process.exit();
 }
 iniciarJuego();
